4daeed379aa309216d08a16f141b2128c2917bb6
mattiarossi578@gmail.com <mattiarossi578@gmail.com>
2020-10-13 09:19:01 +0200

    Prima versione dell'esercizio che effettua la conversione da decimale a binario

---
+++ progetto_primario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace progetto_primario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
